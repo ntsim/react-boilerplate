ebea4f27a3e04c2fd4ae0195357f9590bd95f284
Nicholas Tsim <nick@hiveit.co.uk>
2018-12-23 20:36:50 +0000

    Apply react-hot-loader to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { hot } from 'react-hot-loader/root';
 import styles from './App.module.scss';
 
 class App extends React.Component {
@@ -19,4 +20,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default hot(App);
